ce0e3759bf279f7c9ba9f47dfd6bd133f9f9d641
htduc <duc@ozerside.com>
2015-05-19 18:16:27 +0700

    Change database and add ckeditor

--- HDServices/Controllers/HomeController.cs
+++ HDServices/Controllers/HomeController.cs
@@ -15,15 +15,6 @@ namespace HDServices.Controllers
 
             using (var ctx = new HDContext())
             {
-                //Student stud = new Student() { LastName = "Student", FirstMidName = "Đức", EnrollmentDate = DateTime.Parse("2005-09-01") };
-
-                //ctx.Students.Add(stud);
-
-                //Teacher te = new Teacher() { LastName = "Student", FirstMidName = "Đức", TeacherCode ="123", ClassID= "12a"};
-
-                //ctx.Students.Add(stud);
-                //ctx.Teachers.Add(te);
-                //ctx.SaveChanges();
             }
 
             ViewBag.Title = "Home Page";
--- HDServices/DAL/HDContext.cs
+++ HDServices/DAL/HDContext.cs
@@ -17,20 +17,53 @@ namespace HDServices.DAL
 
         }
 
-        public DbSet<Enrollment> Enrollments { get; set; }
-        public DbSet<Person> Persons { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Teacher> Teachers { get; set; }
+        // main entities.
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<NewsCategory> NewsCategorys { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Page> Pages { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Person>().ToTable("Person");
-            modelBuilder.Entity<Student>().ToTable("Student");
-            modelBuilder.Entity<Teacher>().ToTable("Teacher");
+            // main entities.
+            modelBuilder.Entity<Content>().ToTable("Content");
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Role>().ToTable("Role");
+            modelBuilder.Entity<NewsCategory>().ToTable("NewsCategory");
+            modelBuilder.Entity<News>().ToTable("News");
+            modelBuilder.Entity<Page>().ToTable("Page");
 
-        } 
+            modelBuilder.Entity<User>()
+                  .HasMany<Role>(s => s.Roles)
+                  .WithMany(c => c.Users)
+                  .Map(cs =>
+                  {
+                      cs.MapLeftKey("UserID");
+                      cs.MapRightKey("RoleID");
+                      cs.ToTable("UserRole");
+                  });
+
+            //one-to-many 
+            modelBuilder.Entity<NewsCategory>()
+                        .HasMany<News>(s => s.NewsList)
+                        .WithRequired(s => s.NewsCategory)
+                        .HasForeignKey(s => s.NewsCategoryID);
+
+            //one-to-many  
+            modelBuilder.Entity<Page>()
+                        .HasOptional<Content>(s => s.Content)
+                        .WithMany(s => s.Pages)
+                        .HasForeignKey(s => s.ContentID);
+
+
+            //seft reference 
+            modelBuilder.Entity<Page>().HasOptional(p => p.Parent).WithMany(p => p.Children).HasForeignKey(t => t.ParentID);
+
+
+        }
     }
 }
