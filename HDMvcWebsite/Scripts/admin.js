311b8a08f8086a22734e260bc7139bc8c90e2913
htduc <duc@ozerside.com>
2015-05-21 18:18:24 +0700

    change animation by using js

--- HDMvcWebsite/Scripts/admin.js
+++ HDMvcWebsite/Scripts/admin.js
@@ -1,9 +1,9 @@
 ﻿$(document).ready(function () {
     
-    //$('ul.masterActions li').mouseenter(function () {
-    //    $(this).find('i').animate({ marginLeft: "30px"}, 400);
-    //}).mouseleave(function () {
-    //    $(this).find('i').animate({ marginLeft: "0" }, 400);
-    //});
+    $('ul.masterActions li').mouseenter(function () {
+        $(this).find('i').animate({ marginLeft: "30px"}, 400);
+    }).mouseleave(function () {
+        $(this).find('i').animate({ marginLeft: "0" }, 400);
+    });
 
 });
